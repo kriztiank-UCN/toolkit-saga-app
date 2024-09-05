1820d262f57f862e8f3925d4471726853a93f414
kk <kriztiank@gmail.com>
2024-09-05 10:33:13 +0200

    Create the Jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
-import './App.css';
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getCatsFetch } from "./catState";
+import "./App.css";
 
 function App() {
+  const cats = useSelector((state) => state.cats.cats);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCatsFetch());
+  }, [dispatch]);
+  console.log(cats);
+
   return (
     <div className="App">
-      
+      <h1>CAT SPECIES GALLERY</h1>
+      <p>
+        Images of different species of cats. Lots of cats for your viewing
+        pleasure.
+      </p>
+      <hr />
+      <div className="Gallery">
+        {cats.map((cat) => (
+          <div key={cat.id} className="row">
+            <div className="column column-left">
+              <img
+                src={`https://cdn2.thecatapi.com/images/${cat.reference_image_id}.jpg`}
+                alt={cat.name}
+                width={200}
+                height={200}
+              />
+            </div>
+            <div className="column column-right">
+              <h2>{cat.name}</h2>
+              <h5>{cat.temperament}</h5>
+              <p>{cat.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import "./index.css"
-import App from "./App"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-import { Provider } from "react-redux"
-import { configureStore } from "@reduxjs/toolkit"
-import createSagaMiddleware from "redux-saga"
-import catsReducer from "./catState"
-import catSaga from "./catSaga"
+import { Provider } from "react-redux";
+import createSagaMiddleware from "redux-saga";
+import { configureStore } from "@reduxjs/toolkit";
+import catsReducer from "./catState";
+import catSaga from "./catSaga";
 
-const saga = createSagaMiddleware()
+const saga = createSagaMiddleware();
 const store = configureStore({
   reducer: {
     cats: catsReducer,
   },
-  middleware: [saga],
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(saga),
 });
 saga.run(catSaga);
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <App />
   </Provider>
-)
+);
